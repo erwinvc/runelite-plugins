2e4be9104f27cd81ca7903f7ac6e3177c6cd5709
Erwin van Crasbeek <evancrasbeek@xs4all.nl>
2021-09-02 20:16:43 +0200

    Copied empty plugin branch to clanlist-sort.

--- src/test/java/eu/jodelahithit/placeholder/PlaceholderPluginTest.java
+++ src/test/java/eu/jodelahithit/placeholder/PlaceholderPluginTest.java
@@ -1,13 +1,13 @@
-package eu.jodelahithit;
+package eu.jodelahithit.placeholder;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class SkillingNotificationsTest
+public class PlaceholderPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(SkillingNotificationsPlugin.class);
+		ExternalPluginManager.loadBuiltin(PlaceholderPlugin.class);
 		RuneLite.main(args);
 	}
 }
